8906e57d5c3db489a063665e20d7dc70bfd527af
MidHunterX <midhunterx@gmail.com>
2024-07-30 19:17:15 +0530

    🔗 Created Base URLs

--- client1/clientapp/urls.py
+++ client1/clientapp/urls.py
@@ -1,8 +1,17 @@
-
 from django.urls import path
 from . import views
 
+# Namespace
+app_name = 'emp'
+
+# URL Patterns
 urlpatterns = [
+    # path('', views.welcome_page, name='welcome'),
+    # path('login/', views.login_page, name='login'),
     path('', views.welcome_page, name='welcome'),
-    path('login/', views.login_page, name='login')
+    path('dashboard/',views.dashboard,name = 'dashboard'),
+    path('login/',views.user_login,name = 'user_login'),
+    path('logout/',views.user_logout ,name = 'user_logout'),
+    path('register/',views.register,name = 'register'),
+    path('goodbye/',views.goodbye,name = 'goodbye'),
 ]
