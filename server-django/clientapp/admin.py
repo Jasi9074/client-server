338c6ec755db88c93cfae11231d2c7440e2b60da
MidHunterX <midhunterx@gmail.com>
2024-09-01 15:45:38 +0530

    ⏱️ Added: Admin Word Description Column

--- server-django/clientapp/admin.py
+++ server-django/clientapp/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Employee, WorkSession
 
 class WorkSessionAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'start_time', 'end_time', 'duration')
+    list_display = ('employee', 'start_time', 'end_time', 'duration', 'description')
     list_filter = ('employee', 'start_time', 'end_time')
-    search_fields = ('employee__name', 'start_time', 'end_time')
+    search_fields = ('employee__name', 'start_time', 'end_time', 'description')
 
     def duration(self, obj):
         return obj.duration()
