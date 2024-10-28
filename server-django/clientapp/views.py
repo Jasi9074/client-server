ab0c3aad09d4e3a2778b07a19b9d1db7eb0f74bf
MidHunterX <midhunterx@gmail.com>
2024-10-28 16:58:32 +0530

    💁 Formatted Code with Black

--- server-django/clientapp/views.py
+++ server-django/clientapp/views.py
@@ -13,17 +13,22 @@ from itertools import groupby
 from datetime import date
 import json
 
+
 def server_login(request):
     return render(request, "server/login.html")
+
 
 def login_page(request):
     return render(request, "emp/login.html")
 
+
 def server_page(request):
     return render(request, "server/welcome.html")
 
+
 def client_page():
     return HttpResponseRedirect(reverse("emp:employee_selection"))
+
 
 def register(request):
     if request.user.is_authenticated:
@@ -31,23 +36,21 @@ def register(request):
 
     if request.method == "POST":
         form = UserRegistrationForm(request.POST, request.FILES)
-
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data["password"])
             user.save()
-
             Employee.objects.create(
                 user=user,
                 photo=request.FILES.get("photo"),
             )
-
             return HttpResponseRedirect(reverse("emp:server"))
     else:
         form = UserRegistrationForm()
 
     context = {"form": form}
     return render(request, "emp/register.html", context)
+
 
 @login_required
 def start_work(request):
@@ -61,18 +64,23 @@ def start_work(request):
         employee=employee,
         start_time=timezone.now(),
         description=description,
-        machine=machine_number
+        machine=machine_number,
     )
 
     response = redirect("emp:dashboard")
     if machine_number:
-        response.set_cookie("machine", machine_number, max_age=30*24*60*60)  # 30 days
+        response.set_cookie(
+            "machine", machine_number, max_age=30 * 24 * 60 * 60
+        )  # 30 days
     return response
+
 
 @login_required
 def temp_end_work(request):
     employee = Employee.objects.get(user=request.user)
-    work_session = WorkSession.objects.filter(employee=employee, end_time__isnull=True).last()
+    work_session = WorkSession.objects.filter(
+        employee=employee, end_time__isnull=True
+    ).last()
 
     if work_session:
         work_session.end_time = timezone.now()
@@ -82,7 +90,8 @@ def temp_end_work(request):
     employee.save()
 
     logout(request)
-    return redirect('emp:employee_selection')
+    return redirect("emp:employee_selection")
+
 
 @login_required
 def end_work(request, session_id):
@@ -93,6 +102,7 @@ def end_work(request, session_id):
     session.end_time = timezone.now()
     session.save()
     return redirect("emp:dashboard")
+
 
 @login_required
 def dashboard(request):
@@ -116,11 +126,13 @@ def dashboard(request):
     else:
         return HttpResponseRedirect(reverse("emp:employee_selection"))
 
+
 @login_required
 def user_logout(request):
     username = request.user.first_name + " " + request.user.last_name
     logout(request)
     return HttpResponseRedirect(reverse("emp:goodbye") + f"?username={username}")
+
 
 def goodbye(request):
     username = request.GET.get("username")
@@ -128,62 +140,68 @@ def goodbye(request):
         username = "User"
     return render(request, "emp/goodbye.html", {"username": username})
 
+
 def machine_selection(request):
-    context = {
-        'range': range(1, 25)  # Machines M1 to M24
-    }
+    context = {"range": range(1, 25)}  # Machines M1 to M24
 
     # Get the current employee
     employee = Employee.objects.get(user=request.user)
     # Get the last work session
-    work_session = WorkSession.objects.filter(employee=employee, end_time__isnull=True).last()
+    work_session = WorkSession.objects.filter(
+        employee=employee, end_time__isnull=True
+    ).last()
 
     if work_session:
-        context['issue'] = work_session.issue  # Retrieve the current issue from the work session
+        context["issue"] = work_session.issue  # Retrieve current issue
 
-    return render(request, 'emp/machine_selection.html', context)
+    return render(request, "emp/machine_selection.html", context)
+
 
 def select_machine(request):
-    if request.method == 'POST':
-        selected_machine = request.POST.get('machine')
+    if request.method == "POST":
+        selected_machine = request.POST.get("machine")
         if not selected_machine:
-            return redirect('emp:machine_selection')  # Handle no selection case
+            return redirect("emp:machine_selection")  # Handle no selection case
 
         try:
             employee = Employee.objects.get(user=request.user)
         except Employee.DoesNotExist:
-            return redirect('emp:machine_selection')  # Handle case where Employee is not found
+            return redirect("emp:machine_selection")
 
         # Create a WorkSession
         WorkSession.objects.create(
             employee=employee,
             start_time=timezone.now(),
-            machine=selected_machine  # Assign selected machine
+            machine=selected_machine,  # Assign selected machine
         )
 
         # Redirect to the complaint_selection page with the selected machine number
-        return redirect('emp:complaint_selection', page_id=selected_machine)
+        return redirect("emp:complaint_selection", page_id=selected_machine)
 
-    return redirect('emp:machine_selection')
+    return redirect("emp:machine_selection")
+
 
 def complaint_selection(request, page_id):
     # Retrieve the last work session for the current employee
     employee = Employee.objects.get(user=request.user)
-    work_session = WorkSession.objects.filter(employee=employee, end_time__isnull=True).last()
+    work_session = WorkSession.objects.filter(
+        employee=employee, end_time__isnull=True
+    ).last()
 
     selected_issue = None
     if work_session and work_session.issue:
         selected_issue = work_session.issue  # Fetch the issue from the work session
 
-    complaints = range(1, 13)  # Example complaints; replace with your actual query or model
+    complaints = range(1, 13)
 
     context = {
-        'page_id': page_id,
-        'complaints': complaints,
-        'selected_issue': selected_issue,  # Pass the selected issue to the template
+        "page_id": page_id,
+        "complaints": complaints,
+        "selected_issue": selected_issue,  # Pass the selected issue to the template
     }
 
-    return render(request, 'emp/complaint_selection.html', context)
+    return render(request, "emp/complaint_selection.html", context)
+
 
 def employee_selection(request):
     if request.method == "POST":
@@ -198,7 +216,9 @@ def employee_selection(request):
             if user.is_active:
                 # Log in the user and redirect to the machine selection page
                 login(request, user)
-                request.session.pop('logged_out_employee_username', None)  # Clear the logged_out_employee_username variable
+                request.session.pop(
+                    "logged_out_employee_username", None
+                )  # Clear the logged_out_employee_username variable
                 return HttpResponseRedirect(reverse("emp:machine_selection"))
             else:
                 return HttpResponse("User is not active")
@@ -211,40 +231,48 @@ def employee_selection(request):
     employees = Employee.objects.all()
     employees_with_latest_issues = []
     for employee in employees:
-        latest_work_session = WorkSession.objects.filter(employee=employee).order_by('-start_time').first()
-        employees_with_latest_issues.append({
-            'employee': employee,
-            'session': latest_work_session
-        })
+        latest_work_session = (
+            WorkSession.objects.filter(employee=employee)
+            .order_by("-start_time")
+            .first()
+        )
+        employees_with_latest_issues.append(
+            {"employee": employee, "session": latest_work_session}
+        )
 
-    logged_out_employee_username = request.session.get('logged_out_employee_username', None)
-    return render(request, 'emp/employee_selection.html', {
-        'employees_with_latest_issues': employees_with_latest_issues,
-        'logged_out_employee_username': logged_out_employee_username
-    })
+    logged_out_employee_username = request.session.get(
+        "logged_out_employee_username", None
+    )
+    context = {
+        "employees_with_latest_issues": employees_with_latest_issues,
+        "logged_out_employee_username": logged_out_employee_username,
+    }
+    return render(request, "emp/employee_selection.html", context)
+
 
 def logout_and_redirect(request):
     # Get the username of the logged-out employee and store it in the session
     logged_out_employee_username = request.user.username
-    request.session['logged_out_employee_username'] = logged_out_employee_username
-
+    request.session["logged_out_employee_username"] = logged_out_employee_username
     employee = Employee.objects.get(user=request.user)
     employee.is_working = True
     employee.save()
-
     # Perform the logout
     logout(request)
-    return redirect('emp:employee_selection')
+    return redirect("emp:employee_selection")
+
 
 def save_complaint(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         try:
             data = json.loads(request.body)
-            complaint = data.get('complaint')
-            issue = data.get('issue')
+            complaint = data.get("complaint")
+            issue = data.get("issue")
 
             employee = Employee.objects.get(user=request.user)
-            work_session = WorkSession.objects.filter(employee=employee, end_time__isnull=True).last()
+            work_session = WorkSession.objects.filter(
+                employee=employee, end_time__isnull=True
+            ).last()
 
             if work_session:
                 work_session.complaint = complaint
@@ -252,12 +280,16 @@ def save_complaint(request):
                 work_session.confirmed = True  # Set confirmation status to true
                 work_session.save()
 
-                return JsonResponse({'status': 'success', 'message': 'Complaint saved successfully!'})
+                return JsonResponse(
+                    {"status": "success", "message": "Complaint saved successfully!"}
+                )
             else:
-                return JsonResponse({'status': 'failed', 'message': 'No active work session found.'})
+                return JsonResponse(
+                    {"status": "failed", "message": "No active work session found."}
+                )
         except json.JSONDecodeError:
-            return JsonResponse({'status': 'failed', 'message': 'Invalid JSON data.'})
+            return JsonResponse({"status": "failed", "message": "Invalid JSON data."})
         except Employee.DoesNotExist:
-            return JsonResponse({'status': 'failed', 'message': 'Employee not found.'})
+            return JsonResponse({"status": "failed", "message": "Employee not found."})
 
-    return JsonResponse({'status': 'failed', 'message': 'Invalid request'})
+    return JsonResponse({"status": "failed", "message": "Invalid request"})
